70d54735612ff667c2056dcd18270cf8261c49f9
venya <mymyhighlight@gmail.com>
2017-11-03 20:52:26 +0200

    The hero details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
-  template: 'Hello {{name}}'
-  //templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
+  //template: 'Hello {{title}}, hello {{hero.id}}'
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div>
+  	<label>name: </label>
+  	<input [(ngModel)]="hero.name" placeholder="name">
+  </div>
+  `
 })
 export class AppComponent {
-	name: string = 'World';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
